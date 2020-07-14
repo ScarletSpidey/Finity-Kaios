6ecd276e7e1a44b479b7255255c7561bec930c0a
Shobit Puri <shobitpuri2@gmail.com>
2020-07-15 02:56:33 +0530

    commit

--- finity-kaisos/src/components/Register/Register.js
+++ finity-kaisos/src/components/Register/Register.js
@@ -2,26 +2,37 @@ import React, { Component } from 'react';
 class Register extends Component {
   constructor(props) {
     super(props);
-    this.state = ({
+    this.state = {
       mobilenumber: '',
-    });
+    };
   }
   render() {
     return (
-      <form>
-        <div>
-          <label>Mobile Number</label>
-          <input
-            type='Text'
-            value={this.state.mobilenumber}
-            onChange={(events, newval) =>
-              this.setState({
-                mobilenumber: newval,
-              })
-            }
-          />
+      <div className='container pt-5'>
+        <div className='container'>
+          <form>
+            <div>
+              <label>
+                <small>Mobile Number</small>
+              </label>
+              <input
+                type='Text'
+                className='form-control'
+                value={this.state.mobilenumber}
+                onChange={(events, newval) =>
+                  this.setState({
+                    mobilenumber: newval,
+                  })
+                }
+              />
+              <br />
+              <button type='submit' className='btn btn-primary btn-sm'>
+                Submit
+              </button>
+            </div>
+          </form>
         </div>
-      </form>
+      </div>
     );
   }
 }
